b36867a588dd66ba8d2ec542000b6833850d0fb0
Rutvik <Rutvik@DESKTOP-QN634CE>
2020-12-01 18:16:29 +0530

    added sourcecontrolform

---
+++ SourceControlForm/SourceControlForm/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using SourceControlForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SourceControlForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.SuccessMsg = "<script> alert('Form Submitted SuccessFully')</script>";
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
