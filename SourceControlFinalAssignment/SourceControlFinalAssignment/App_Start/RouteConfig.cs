a290b5eecaae5bb7be8149fdb26c9a1d8d94d7aa
Rutvik <rutvikrupapara56@gmail.com>
2020-12-20 14:46:30 +0530

    Bootstrap added using nuGet

--- SourceControlFinalAssignment/SourceControlFinalAssignment/App_Start/RouteConfig.cs
+++ SourceControlFinalAssignment/SourceControlFinalAssignment/App_Start/RouteConfig.cs
@@ -12,11 +12,12 @@ namespace SourceControlFinalAssignment
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "students", action = "login", id = UrlParameter.Optional }
             );
         }
     }
