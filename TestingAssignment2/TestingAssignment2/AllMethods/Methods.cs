0f1ef4faddeca2bc6705b6109bd908de79753c09
Rutvik Rupapara <rutvikrupapara56@gmail.com>
2021-03-02 23:35:37 +0530

    Testing Assignment 2

---
+++ TestingAssignment2/TestingAssignment2/AllMethods/Methods.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TestingAssignment2.AllMethods
+{
+    public class Methods
+    {
+        public static string UpperLower(string id)
+        {
+            if (id != null && id!="")
+            {
+                if (id.All(char.IsUpper))
+                {
+                    id = id.ToLower();
+                }
+                else
+                {
+                    id = id.ToUpper();
+                }
+                return id;
+            }
+            return null;
+           
+        }
+
+        
+        public static  string TitleCase(string id)
+        {
+            if (id != null)
+            {
+
+                CultureInfo.CurrentCulture.TextInfo.ToTitleCase(id);
+
+                return id;
+            }
+            return null;
+            
+        }
+
+        
+        public static  bool FindLower(string id)
+        {
+            if (id != null && id!="")
+            {
+                if (id.All(char.IsLower))
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+            
+        }
+
+       
+        public static  string FirstUpper(string id)
+        {
+            if (id != null && id != "")
+            {
+                id = id[0].ToString().ToUpper() + id.Substring(1);
+                return id;
+            }
+            return null;
+        }
+
+        
+        public static  bool FindUpper(string id)
+        {
+            if (id != null && id != "")
+            {
+                if (id.All(char.IsUpper))
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+
+       
+        public static  bool FindNumber(string id)
+        {
+            if (id != null && id != "")
+            {
+                var isNumeric = int.TryParse(id, out _);
+                if (isNumeric)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+       
+        public static  string RemoveLast(string id)
+        {
+            if (id != null && id != "")
+            {
+                id = id.Substring(0, id.Length - 1);
+
+                return id;
+            }
+            return null;
+        }
+
+        
+        public static  int WordCount(string id)
+        {
+            if (id != null && id != "")
+            {
+
+                var match = Regex.Matches(id,
+                    @"[^\s.?,]+", RegexOptions.IgnoreCase);
+
+                return match.Count;
+            }
+            return 0;
+        }
+
+        
+        public static  int? ConvertToInt(string id)
+        {
+            if (id != null && id != "")
+            {
+                var isNumeric = int.TryParse(id, out _);
+                if (isNumeric)
+                {
+                    var c = Convert.ToInt32(id);
+
+                    return c;
+                }
+                return null;
+            }
+            return null;
+        }
+    }
+}
---
+++ TestingAssignment2/UnitTesting/StringMethodsTesting.cs
@@ -0,0 +1,178 @@
+﻿using TestingAssignment2.AllMethods;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class MethodsTesting
+    {
+        [Theory]
+        [InlineData("teststring", "TESTSTRING")]
+        [InlineData("TESTSTRING", "teststring")]
+        [InlineData("test123", "TEST123")]
+        [InlineData("", null)]
+        [InlineData(null, null)]
+        public void UpperLower(string str, string expected)
+        {
+            //Arrange
+
+            //Act
+            var result = Methods.UpperLower(str);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("TEST", "TEST")]
+        [InlineData("TEST123", "TEST123")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void TitleCase(string str, string expected)
+        {
+            //Arrange
+
+            //Act
+            var result = Methods.TitleCase(str);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("test", true)]
+        [InlineData("test lower case", false)] //Space is not a LowerCase Character
+        [InlineData("TEST", false)]
+        [InlineData("TEST123", false)]
+        [InlineData("test123", false)] //Not all characters are LowerCase
+        [InlineData("123", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void FindLower(string str, bool expected)
+        {
+            //Arrange
+
+            //Act
+            var result = Methods.FindLower(str);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("TEST", true)]
+        [InlineData("TEST TITLE CASE", false)] //Space is not an UpperCase Character
+        [InlineData("test", false)]
+        [InlineData("test123", false)]
+        [InlineData("TEST123", false)] //Not all characters are UpperCase
+        [InlineData("123", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void FindUpper(string str, bool expected)
+        {
+            //Arrange
+
+            //Act
+            var result = Methods.FindUpper(str);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("test", "Test")]
+        [InlineData("TEST", "TEST")]
+        [InlineData("test test test", "Test test test")]
+        [InlineData("tEST", "TEST")]
+        [InlineData("test123", "Test123")]
+        [InlineData("123", "123")]
+        [InlineData("", null)]
+        [InlineData(null, null)]
+        public void FirstUpper(string str, string expected)
+        {
+            //Arrange
+
+            //Act
+            var result = Methods.FirstUpper(str);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("test", "tes")]
+        [InlineData("TEST", "TES")]
+        [InlineData("1234", "123")]
+        [InlineData("", null)]
+        [InlineData("t", "")]
+        [InlineData(null, null)]
+        public void RemoveLast(string str, string expected)
+        {
+            //Arrange
+
+            //Act
+            var result = Methods.RemoveLast(str);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("test", 1)]
+        [InlineData("test words", 2)]
+        [InlineData("", 0)]
+        [InlineData(" ", 0)]
+        [InlineData("  ", 0)]
+        [InlineData("Test words  ", 2)]
+        [InlineData(null, 0)]
+        public void WordCount(string str, int expected)
+        {
+            //Arrange
+
+            //Act
+            var result = Methods.WordCount(str);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("1", true)]
+        [InlineData("0", true)]
+        [InlineData("-1", true)]
+        [InlineData("00", true)]
+        [InlineData("", false)]
+        [InlineData("a", false)]
+        [InlineData("0a", false)]
+        [InlineData(null, false)]
+        public void FindNumber(string str, bool expected)
+        {
+            //Arrange
+
+            //Act
+            var result = Methods.FindNumber(str);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("0", 0)]
+        [InlineData("-1", -1)]
+        [InlineData("00", 00)]
+        [InlineData("", null)]
+        [InlineData("a", null)]
+        [InlineData("0a", null)]
+        [InlineData(null, null)]
+        public void ConvertToInt(string str, double? expected)
+        {
+            //Arrange
+
+            //Act
+            var result = Methods.ConvertToInt(str);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
